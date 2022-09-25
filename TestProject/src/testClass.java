e10d992bda2af47e65553431278eaf6be0d31673
Jzash <Jzash@10.1.190.206>
2022-09-25 14:13:51 -0400

    test class committing

---
+++ TestProject/src/testClass.java
@@ -0,0 +1,15 @@
+
+public class testClass {
+	public static void main(String[] args) { 
+		
+		double a = 12.5;
+		double b = 13.5; 
+		double area = a*b; 
+		
+		System.out.println(area);
+			
+		
+	}
+	
+	
+}
